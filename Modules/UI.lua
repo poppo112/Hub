c545c3a9257cf963f3c518d6ebabbfd93d87d0f8
poppo112 <144562052+poppo112@users.noreply.github.com>
2023-09-10 04:24:11 +0700

    Update UI.lua

--- Modules/UI.lua
+++ Modules/UI.lua
@@ -1,7 +1,7 @@
 local UI = {}
 
 -- Variables
-local Library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/poppo112/Hub/main/source')))()
 local Window = Library:MakeWindow({Name = "Parkour by xgamer626#1683", HidePremium = true})
 
 local Combo_Tab = Window:MakeTab({

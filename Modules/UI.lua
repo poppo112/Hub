9202a63c913ad08cfc64b6c640adb47528dfcb3d
poppo112 <144562052+poppo112@users.noreply.github.com>
2023-09-10 04:37:25 +0700

    Update UI.lua

--- Modules/UI.lua
+++ Modules/UI.lua
@@ -2,7 +2,7 @@ local UI = {}
 
 -- Variables
 local Library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/poppo112/Hub/main/source')))()
-local Window = Library:MakeWindow({Name = "Parkour by xgamer626#1683", HidePremium = true})
+local Window = Library:MakeWindow({Name = "pepe97", HidePremium = true})
 
 local Combo_Tab = Window:MakeTab({
     Name = "Combo",
